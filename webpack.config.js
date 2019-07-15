4974ffd6023a18e4e819fce5c53f59ecb8ab6cf9
dyyfk <879608095@qq.com>
2019-07-14 21:44:09 -0500

    init

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+let stylusLoader = {
+    loader: 'stylus-relative-loader'
+};
+
+let jsLoader = {
+    loader: 'babel-loader',
+    options: {
+        presets: ['@babel/preset-env', '@babel/preset-react'],
+    }
+}
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        rules: [
+            { test: /\.(js|jsx)$/, use: [jsLoader], exclude: /node_modules/ },
+            { test: /\.coffee$/, use: [jsLoader, "coffee-loader"] },
+            { test: /\.styl$/, use: ["style-loader", "css-loader", stylusLoader] },
+            { test: /\.css$/, use: ["style-loader", 'css-loader'] }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+}
